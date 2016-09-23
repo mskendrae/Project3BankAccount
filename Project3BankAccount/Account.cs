c19395e9f1579b231f7bc65a82392cf4b025be20
Kendra Egert <mskendrae@gmail.com>
2016-09-23 14:57:05 -0400

    initial commit

---
+++ Project3BankAccount/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3BankAccount
+{
+    class Account
+    {
+
+        //This is the account class for Gringots Wizarding Bank and defines the common
+        //traits for all accounts for the account holder.
+
+
+         //fields
+        private string acctName;
+        private string acctNumber;
+        private string acctType;
+        private double acctbalance;
+
+        //properties
+
+
+
+
+        //contructors
+
+        public Account(string acctName, string acctNumber, string acctType)
+        {
+            this.acctName = acctName;
+            this.acctNumber = acctNumber;
+            this.acctType = acctType;
+
+
+        }
+
+        //methods
+
+        public double WithdrawalGold(double totalBalance, string acctType)
+        {
+            double removeGold = 
+
+        }
+
+
+
+    }
+}
---
+++ Project3BankAccount/Spend.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3BankAccount
+{
+    class Spend:Account
+
+    {
+        //This is muggle worlds Checking account - WE dont have "checks"
+        //all monies are in wizard world gold 
+
+
+
+
+    }
+}
